72a7ed3a9a5a371e8efb92451760fcde15b43315
shoesprite <shoesprite@users.noreply.github.com>
2015-01-11 15:01:43 -0800

    Update dragongame.js

--- dragongame.js
+++ dragongame.js
@@ -4,22 +4,22 @@ var youHit = Math.floor(Math.random() * 2);
 var damageThisRound = Math.floor(Math.random() * 5 + 1);
 var totalDamage = 0;
 var hi = document.getElementById('hi')
-
-while (slaying) {
+var button = document.getElementById('button')
+button.onclick = function () {
   if (youHit) {
   var textnode = document.createTextNode("You hit the dragon and did " + damageThisRound + " damage!");
   hi.appendChild(textnode)
     totalDamage += damageThisRound;
     
     if (totalDamage >= 4) {
-      var textnode1 = document.createTextNode("You did it! You slew the dragon!");
+      var textnode1 = document.createTextNode(" You did it! You slew the dragon!");
       hi.appendChild(textnode1)
       slaying = false;
     } else {
       youHit = Math.floor(Math.random() * 2);
     }
   } else {
-      var textnode2 = document.createTextNode("The dragon burninates you! You're toast.");
+      var textnode2 = document.createTextNode(" The dragon burninates you! You're toast.");
       hi.appendChild(textnode2)
     slaying = false;
   }
