388d0dd3f044297808b98b611725214088decaee
shoesprite <shoesprite@users.noreply.github.com>
2015-03-05 18:23:26 -0800

    Update adventure.js

--- adventure.js
+++ adventure.js
@@ -1,16 +1,25 @@
+window.onload = function () {
+  var adventure_text = document.getElementById('adventure_text')
+  var yesButton = document.getElementById('yesButton')
+  var noButton = document.getElementById('noButton')
+
+}
  confirm("Are you ready to play?")
 var money = 0
-confirm("You are an adventurer, trekking through the forest in search of monsters and treasure. ");
-var userAnswer = prompt("Suddenly, you find a baby dragon! Dragon hide is very valuable, but the hatchling is so adorable. It stares at you with big eyes, and starts to follow you around wherever you go. It looks like you can't just walk away. Do you kill the dragon hatchling and sell its hide, or keep it as a pet?")
-if (userAnswer == 'keep')
-{
-    confirm("You pat the the hatchling on its head. It purrs, leaving soot on your pants.")
-} else {
-    confirm("You walk away from the deal with heavy pockets and a heavy heart. The dragon's eyes linger in your mind.");
-    money = 100
-    var userAnswerII = prompt("On the bright side, you have 100gp to spend. Would you like to buy anything?")
+var textnode = document.createTextNode("You are an adventurer, trekking through the forest in search of monsters and treasure. ");
+adventure_text.appendChild(textnode)
+var textnode2 = document.createTextNode("Suddenly, you find a baby dragon! Dragons are a lot of work, but the hatchling is so adorable. It stares at you with big eyes, and starts to follow you around wherever you go. It looks like you can't just walk away. Do you keep it as a pet?")
+
+ yesButton.onclick = function() {
+  var textnode3 = document.createTextNode("You pat the the hatchling on its head. It purrs, leaving soot on your pants.")
+  adventure_text.appendChild(textnode3)
+} noButton.onclick = function() {
+  var textnode4 = document.createTextNode("You walk away from the deal with heavy pockets and a heavy heart. The dragon's eyes linger in your mind.");
+  adventure_text.appendChild(textnode4)
+  money = 100
+  var userAnswerII = prompt("On the bright side, you have 100gp to spend. Would you like to buy anything?")
 }
-var feedback = prompt("Please rate my game out of 10.")
+//var feedback = prompt("Please rate my game out of 10.")
 if (feedback > 8)
 {
     var textnode = document.createTextNode("Thank you for your feedback; I'm glad you like my game!")
